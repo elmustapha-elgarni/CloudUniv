8f965c117864b393cde70ec758b3d801ba539b31
elmustapha-elgarni <63479420+elmustapha-elgarni@users.noreply.github.com>
2020-05-20 00:43:03 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ docker rm $(docker ps -aq)
 
 docker run --name mysql -d -e MYSQL_RANDOM_ROOT_PASSWORD=yes -e MYSQL_DATABASE=flaskblog -e MYSQL_USER=admin -e MYSQL_PASSWORD=user mysql/mysql-server:5.7
 
-docker run --name webapp  -p 80:5000 --rm -e SECRET_KEY=my-secret-key -e MAIL_SERVER=smtp.googlemail.com -e MAIL_PORT=587 -e MAIL_USE_TLS=true -e MAIL_USERNAME=elmustapha.elgarni09@gamil.com -e MAIL_PASSWORD=Testing2020 --link mysql:dbserver -e SQLALCHEMY_DATABASE_URI=mysql+pymysql://admin:user@dbserver:3306/flaskblog flask:latest
+docker run --name webapp  -p 80:5000 --rm -e SECRET_KEY=my-secret-key -e MAIL_SERVER=smtp.googlemail.com -e MAIL_PORT=587 -e MAIL_USE_TLS=true -e MAIL_USERNAME=elmustapha.elgarni09@gmail.com -e MAIL_PASSWORD=Testing2020 --link mysql:dbserver -e SQLALCHEMY_DATABASE_URI=mysql+pymysql://admin:user@dbserver:3306/flaskblog flask:latest
 
